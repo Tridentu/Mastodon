81796c1b4ddd3a3e04e7b6c19d8bbbcf4ddb27d4
Aerodos12 <weldon.henson97@gmail.com>
2021-04-15 18:20:21 -0400

    Added Users, Sessions and Messages

--- js/mastodon.js
+++ js/mastodon.js
@@ -1,6 +1,5 @@
 
 $(function(){
    // let user_names = lightdm.users;
-   $(".mastodon-user-select").select2();
-   $(".mastodon-language-select").select2();
+   $(".mastodon-select > select").select2();
 });
