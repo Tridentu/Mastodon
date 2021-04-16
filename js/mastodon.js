8f54821aad22b87a0c5d3b731e89048fba3fc7bf
Aerodos12 <weldon.henson97@gmail.com>
2021-04-15 20:18:44 -0400

    Start of JS

--- js/mastodon.js
+++ js/mastodon.js
@@ -2,4 +2,11 @@
 $(function(){
    // let user_names = lightdm.users;
    $(".mastodon-select > select").select2();
+   
+   $("#restart").click(lightdm.shutdown());
+   $("#shutdown").click(lightdm.shutdown());
+   $("#sleep").click(lightdm.suspend());
+   $("#hibernate").click(lightdm.hibernate());
+   
+
 });
