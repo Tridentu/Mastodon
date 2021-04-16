e8b9aaba6e61d8fdffb33480a1bf8c30292228e3
Aerodos12 <weldon.henson97@gmail.com>
2021-04-16 11:17:09 -0400

    Added show_prompt and show_message (JS).

--- js/mastodon.js
+++ js/mastodon.js
@@ -1,12 +1,48 @@
 
+window.show_message = function(text, type){
+    if(0 === text.length)
+        return;
+    if(type == "error")
+        text = `<span class="mastodon-error">${text}</span>`;
+    
+    $(".mastodon-messages").append(text);
+    $("mastodon-messages").fadeIn(700);
+    
+};
+
+window.show_prompt = function(text, type){
+    let entry = $(".mastodon-login");
+    entry.val("");
+    entry.attr("type",type);
+    entry.attr("placeholder", text.charAt(0).toUpperCase() + text.slice(1, -1);
+};
+
 $(function(){
    // let user_names = lightdm.users;
+   $(".mastodon-layout-select").forEach((item) => {
+      lightdm.layouts.forEach((item2,index) => {
+        var opt = new Option(item2.description, item2.name);
+        $(opt).html(item2.description);
+        item.append(opt);
+      });
+   });
+   $("#restart").click(function(){
+       if(lightdm.can_restart) 
+           lightdm.shutdown();
+   });
+   $("#sleep").click(function(){
+       if(lightdm.can_suspend) 
+            lightdm.suspend();
+   });
+   $("#shutdown").click(function(){
+        if(lightdm.can_shutdown) 
+            lightdm.shutdown();
+   });
+   $("#hibernate").click(function(){
+        if(lightdm.can_hibernate) 
+            lightdm.hibernate();
+   });   
+   $(".mastodon-messages").html("");
    $(".mastodon-select > select").select2();
-   
-   $("#restart").click(lightdm.shutdown());
-   $("#shutdown").click(lightdm.shutdown());
-   $("#sleep").click(lightdm.suspend());
-   $("#hibernate").click(lightdm.hibernate());
-   
 
 });
